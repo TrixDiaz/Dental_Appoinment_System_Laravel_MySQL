8058ce9be828dd6692a21e69320d9a2921b9d694
XirTech <john.darlucio022@gmail.com>
2023-05-20 10:22:27 +0800

    fix

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -20,7 +20,7 @@ class AdminSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'role' => 'admin',
             'type' => 'doctor',
-            'profile_photo_path' => '/image/avatar.png'
+        
         ]);
     }
 }
